7e8c48c91dbf72d89e0dd0427b2f14f0223a09c7
Mansuy Léo <169048834+MansuyLeo@users.noreply.github.com>
2024-10-02 19:03:52 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 @echo off
-powershell -ExecutionPolicy Bypass -NoExit -File "%~dp0SecureBoot_Lenovo_v1_1.ps1"
+powershell -ExecutionPolicy Bypass -NoExit -File "%~dp0SecureBoot_Lenovo_v1_1.ps1" -Verb RunAs
